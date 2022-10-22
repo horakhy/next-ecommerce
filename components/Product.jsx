403fee9f8bd01f46934951ac0d105c78bc1550ab
horakhy <leonidas51000@hotmail.com>
2022-10-22 13:40:57 -0300

    feat: product component for Home

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { urlFor } from "../lib/client";
 
-const Product = () => {
+const Product = ({
+  key,
+  product: { image, name, slug, price, description },
+}) => {
   return (
-    <div>Product</div>
-  )
-}
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+            alt="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
 
-export default Product
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Product;
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ const Home = ({ products, bannerData}) => {
         <p>Best Speakers in town</p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
       <FooterBanner />
     </>
